f9b958799ce7624ab37da707eabb65075a801ff0
Chad-Mowbray <mowbray.chad@gmail.com>
2019-07-03 13:46:16 -0500

    six

--- app.py
+++ app.py
@@ -15,66 +15,66 @@ def hello_world():
 # app = flask.Flask(__name__)
 #app.config["DEBUG"] = True
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+# def dict_factory(cursor, row):
+#     d = {}
+#     for idx, col in enumerate(cursor.description):
+#         d[col[0]] = row[idx]
+#     return d
 
 
-@app.route('/', methods=['GET'])
-def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+# @app.route('/', methods=['GET'])
+# def home():
+#     return '''<h1>Distant Reading Archive</h1>
+# <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM books;').fetchall()
+# @app.route('/api/v1/resources/books/all', methods=['GET'])
+# def api_all():
+#     conn = sqlite3.connect('books.db')
+#     conn.row_factory = dict_factory
+#     cur = conn.cursor()
+#     all_books = cur.execute('SELECT * FROM books;').fetchall()
 
-    return jsonify(all_books)
-
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+#     return jsonify(all_books)
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
 
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
 
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return page_not_found(404)
+# @app.route('/api/v1/resources/books', methods=['GET'])
+# def api_filter():
+#     query_parameters = request.args
 
-    query = query[:-4] + ';'
+#     id = query_parameters.get('id')
+#     published = query_parameters.get('published')
+#     author = query_parameters.get('author')
 
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
+#     query = "SELECT * FROM books WHERE"
+#     to_filter = []
 
-    results = cur.execute(query, to_filter).fetchall()
+#     if id:
+#         query += ' id=? AND'
+#         to_filter.append(id)
+#     if published:
+#         query += ' published=? AND'
+#         to_filter.append(published)
+#     if author:
+#         query += ' author=? AND'
+#         to_filter.append(author)
+#     if not (id or published or author):
+#         return page_not_found(404)
 
-    return jsonify(results)
+#     query = query[:-4] + ';'
 
-app.run()
+#     conn = sqlite3.connect('books.db')
+#     conn.row_factory = dict_factory
+#     cur = conn.cursor()
+
+#     results = cur.execute(query, to_filter).fetchall()
+
+#     return jsonify(results)
+
+# app.run()
