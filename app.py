fdeb15ba0b4324a6e63a7c8b1492bc8ec77fc6a9
Chad-Mowbray <mowbray.chad@gmail.com>
2019-07-04 09:50:53 -0500

    initial

--- app.py
+++ app.py
@@ -10,70 +10,103 @@
 import flask
 from flask import request, jsonify
 import sqlite3
+import markovify
+
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+# def dict_factory(cursor, row):
+#     d = {}
+#     for idx, col in enumerate(cursor.description):
+#         d[col[0]] = row[idx]
+#     return d
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return '''<h1>Distant Reading Archive</h1>
-<p>A prototype API for distant reading of science fiction novels.</p>'''
+#     return '''<h1>Distant Reading Archive</h1>
+# <p>A prototype API for distant reading of science fiction novels.</p>'''
 
 
-@app.route('/api/v1/resources/books/all', methods=['GET'])
-def api_all():
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-    all_books = cur.execute('SELECT * FROM books;').fetchall()
+    # Get raw text as string.
+    with open("combo.txt") as f:
+        text = f.read()
 
-    return jsonify(all_books)
+    
 
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
+    # Build the model.
+    text_model = markovify.Text(text, state_size=4)
+    #text_model_b = markovify.Text(goosebumps, state_size=4)
 
 
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
+    # model_combo = markovify.combine([text_model_a, text_model_b], [1, 1.3])
 
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
 
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
+    # Print five randomly-generated sentences
+    arr = []
+    for i in range(4):
+        result = text_model.make_sentence(tries=10000, max_overlap_total=15)
+        arr.append(result)
 
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return page_not_found(404)
+    json_result = jsonify(arr)
 
-    query = query[:-4] + ';'
+    return json_result
 
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
 
-    results = cur.execute(query, to_filter).fetchall()
 
-    return jsonify(results)
 
-# app.run()
+
+
+
+
+# @app.route('/api/v1/resources/books/all', methods=['GET'])
+# def api_all():
+#     conn = sqlite3.connect('books.db')
+#     conn.row_factory = dict_factory
+#     cur = conn.cursor()
+#     all_books = cur.execute('SELECT * FROM books;').fetchall()
+
+#     return jsonify(all_books)
+
+
+
+# @app.errorhandler(404)
+# def page_not_found(e):
+#     return "<h1>404</h1><p>The resource could not be found.</p>", 404
+
+
+# @app.route('/api/v1/resources/books', methods=['GET'])
+# def api_filter():
+#     query_parameters = request.args
+
+#     id = query_parameters.get('id')
+#     published = query_parameters.get('published')
+#     author = query_parameters.get('author')
+
+#     query = "SELECT * FROM books WHERE"
+#     to_filter = []
+
+#     if id:
+#         query += ' id=? AND'
+#         to_filter.append(id)
+#     if published:
+#         query += ' published=? AND'
+#         to_filter.append(published)
+#     if author:
+#         query += ' author=? AND'
+#         to_filter.append(author)
+#     if not (id or published or author):
+#         return page_not_found(404)
+
+#     query = query[:-4] + ';'
+
+#     conn = sqlite3.connect('books.db')
+#     conn.row_factory = dict_factory
+#     cur = conn.cursor()
+
+#     results = cur.execute(query, to_filter).fetchall()
+
+#     return jsonify(results)
+
+app.run()
